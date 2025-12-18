634bfc425a08824dadeb22e021774eb344f69e87
bala-amypo <bala@amypo.in>
2025-12-18 06:34:11 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,10 +5,39 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.demo.entity.StudentEntity;
-import com.example.demo.service.StudentService;
+import com.example.demo.entity.UserEntity;
+import com.example.demo.service.UserService;
 
-@Restcontroller
+@RestController
 @RequestMapping("/User")
+public class UserController {
 
+    @Autowired
+    private UserService service;
 
+    // ADD
+    @PostMapping("/add")
+    public UserEntity addStudent(@RequestBody UserEntity student) {
+        return service.addStudent(student);
+    }
+
+    // GET ALL
+    @GetMapping("/all")
+    public List<UserEntity> getAllStudents() {
+        return service.getAllStudents();
+    }
+
+    // GET BY ID
+    @GetMapping("/{id}")
+    public UserEntity getStudentById(@PathVariable Long id) {
+        return service.getStudentById(id);
+    }
+
+    // UPDATE
+    @PutMapping("/update/{id}")
+    public UserEntity updateStudent(
+            @PathVariable Long id,
+            @RequestBody StudentEntity student) {
+        return service.updateStudent(id, student);
+    }
+}
